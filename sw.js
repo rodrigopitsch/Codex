4b3deba62a1a4cb47af0136ca213f9101bda52ff
rodrigopitsch <rodrigopitsch@icloud.com>
2026-08-16 15:01:10 -0300

    corrige precache offline da Sala de Jogos

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='sala-jogos-offline-v1';
-const FILES=['./','./index.html','./jogo-da-velha.html','./ludo.html','./paciencia-trilha.html','./hunt.html','./occult-trail.html','./manifest.webmanifest','./icon.svg'];
+const CACHE='sala-jogos-offline-v2';
+const FILES=['./index.html','./jogo-da-velha.html','./ludo.html','./paciencia-trilha.html','./hunt.html','./occult-trail.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
